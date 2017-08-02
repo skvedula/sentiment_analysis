ad78215ce48d0e297356893484d9b0c5e3caa205
skvedula <skvedula@outlook.com>
2017-08-02 17:22:11 +0530

    WIP: refactored a bit more, will not work

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,75 +1,52 @@
+import re
 import sys
 
+SentimentWordBuffer = 5
+
 class SentimentAnalysis:
+
 	def __init__(self, input): # this method creates the class object.
 	    self.input = input
 
-	def getSentimentforText(data):
-
-		double probs[]={0.0,0.0};
-		String sentences[] = data.split("[,.?\\n]");
-		if(sentences.length>0)
-		{
-			String tokens[] = sentences[sentences.length-1].split(" ");
-			for(int i=tokens.length-1;i>=tokens.length-SentimentWordBuffer && i>=0 ;i--)
-			{
-				tokens[i] = normalizeSentimentWord(tokens[i]);
-			}
-			for(int i=tokens.length-1;i>=tokens.length-SentimentWordBuffer && i>=0 ;i--)
-			{
-				if(tokens[i]!=null)
-				{
-					if(isTokenConjugation(tokens[i]))
-					{
-						break;
-					}
-					int idx = searchSentimentKeyword(tokens[i]);
-					if(idx>=0)
-					{
-						probs[0]+= ((double)UnigramModelElementList.get(idx).negativeScore/1000.0);
-						probs[1]+= ((double)UnigramModelElementList.get(idx).positiveScore/1000.0);	
-					}
-					if(i>0)
-					{
-						int previdx = searchSentimentKeyword(tokens[i-1]);
-						if(idx>=0&&previdx>=0)
-						{
-							
-							int bigramidx = searchBigramKeyword(previdx,idx);
-							//System.out.println(tokens[i-1]+" "+ tokens[i]);
-							//System.out.println(previdx+" "+ idx+" "+bigramidx);
-							if(bigramidx>=0)
-							{
-								probs[0]+= ((double)BigramModelElementList.get(bigramidx).negativeScore/1000.0);
-								probs[1]+= ((double)BigramModelElementList.get(bigramidx).positiveScore/1000.0);	
-							}
-						}
-					}
-				}
-			}
-			double probsum = sumLogProb(probs);
-			double probnegative = Math.exp(probs[0])/Math.exp(probsum);
-			double probpositive = Math.exp(probs[1])/Math.exp(probsum);
-			outputSentimentElement.negProb = probnegative;
-			outputSentimentElement.posProb = probpositive;
-			if(probnegative>probpositive)
-			{
-				outputSentimentElement.Sentiment = "NEGATIVE";
-			}
-			else if(probnegative<probpositive)
-			{
-				outputSentimentElement.Sentiment = "POSITIVE";
-			}
-			else 
-			{
-				outputSentimentElement.Sentiment = "NEUTRAL";
-			}
-			return probnegative;
-		}
-		return defaultnegativeprobability;
+	def getSentimentforText(self, data):
+		probs = [0,0];
+		data = data.replace("\\n", ",")
+		sentences = re.split('[|,|.|?]', data)
+		if(sentences.length > 0):
+				tokens = sentences[len(sentences)-1].split(" ")
+				for(i=tokens.length-1; i>= tokens.length - SentimentWordBuffer && i>=0; i--)
+					tokens[i] = normalizeSentimentWord(tokens[i])
+				for(i=tokens.length-1;i>=tokens.length-SentimentWordBuffer && i>=0 ;i--)
+					if(tokens[i]!=null):
+						if(isTokenConjugation(tokens[i])):
+							break
+						idx = searchSentimentKeyword(tokens[i])
+						if(idx>=0):
+							probs[0] += UnigramModelElementList.get(idx).negativeScore/1000.0
+							probs[1] += UnigramModelElementList.get(idx).positiveScore/1000.0
+						if(i>0):
+							previdx = searchSentimentKeyword(tokens[i-1])
+							if(idx>=0&&previdx>=0):
+								bigramidx = searchBigramKeyword(previdx,idx)
+								if(bigramidx>=0):
+									probs[0]+= BigramModelElementList.get(bigramidx).negativeScore/1000.0
+									probs[1]+= BigramModelElementList.get(bigramidx).positiveScore/1000.0
+				probsum = sumLogProb(probs)
+				probnegative = Math.exp(probs[0])/Math.exp(probsum)
+				probpositive = Math.exp(probs[1])/Math.exp(probsum)
+				outputSentimentElement.negProb = probnegative
+				outputSentimentElement.posProb = probpositive
+				if(probnegative>probpositive):
+					outputSentimentElement.Sentiment = "NEGATIVE"
+				elif(probnegative<probpositive):
+					outputSentimentElement.Sentiment = "POSITIVE"
+				else:
+					outputSentimentElement.Sentiment = "NEUTRAL"
+				return probnegative
+		return defaultnegativeprobability
 
 	def runIntentAnalyzer(self):
-		getSentimentforText(self.text)
+		self.getSentimentforText(self.input)
 		obj = {}
 		obj["negProb"] = 0.8638921090281604
 		obj["posProb"] = 0.13610789097183956
